56472e9dc50e4b103f286fde8b3f0bd32ffd4990
johnlinp <johnlinp@gmail.com>
2017-02-07 23:39:44 +0800

    Fake bot interactions

--- bot.js
+++ bot.js
@@ -11,9 +11,76 @@ if (process.env.NODE_ENV === 'production') {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
+var parseSentence = function(sentence) {
+    var matches;
+
+    matches = /^\/start/i.exec(sentence);
+    if (matches) {
+        return {
+            action: 'START',
+        };
+    }
+
+    matches = /^\/help/i.exec(sentence);
+    if (matches) {
+        return {
+            action: 'HELP',
+        };
+    }
+
+    matches = /^(.+) when i am (.+)$/i.exec(sentence);
+    if (matches) {
+        return {
+            action: 'ADD-TODO',
+            todo: matches[1],
+            timing: matches[2],
+        };
+    }
+
+    matches = /^i am (.+)$/i.exec(sentence);
+    if (matches) {
+        return {
+            action: 'WHAT-TO-DO',
+            timing: matches[1],
+        };
+    }
+
+    matches = /^done$/i.exec(sentence);
+    if (matches) {
+        return {
+            action: 'REMOVE-TODO',
+        };
+    }
+
+    return {
+        action: 'UNKNOWN'
+    };
+};
+
 bot.onText(/(.+)/, function (msg, match) {
-    var content = match[1];
-    bot.sendMessage(msg.chat.id, 'You said: "' + content + '"');
+    var sentence = match[1];
+    var args = parseSentence(sentence);
+
+    switch (args.action) {
+        case 'ADD-TODO':
+            bot.sendMessage(msg.chat.id, 'Okay, I will remind you to ' + args.todo + ' when you are ' + args.timing + '.');
+            break;
+        case 'WHAT-TO-DO':
+            bot.sendMessage(msg.chat.id, 'Go buy some socks.');
+            break;
+        case 'REMOVE-TODO':
+            bot.sendMessage(msg.chat.id, 'Great job!');
+            break;
+        case 'START':
+            bot.sendMessage(msg.chat.id, 'Welcome!\nType "buy some socks when i am at some store" or type /help to see the usage.');
+            break;
+        case 'HELP':
+            bot.sendMessage(msg.chat.id, 'I can understand the following patterns:\n\n1. [do something] when I am [some context]\n2. I am [some context]\n3. done');
+            break;
+        case 'UNKNOWN':
+            bot.sendMessage(msg.chat.id, 'What?');
+            break;
+    }
 });
 
 module.exports = bot;
