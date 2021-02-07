7599f7eb926e00f7c47487c7b47dbb3a095b49d3
John Lin <johnlinp@gmail.com>
2021-02-07 12:18:00 +0800

    Rewrite in python from scratch

---
+++ app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import logging
+import telegram.ext
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def start_command(update, context):
+    update.message.reply_text('Start!')
+
+
+def help_command(update, context):
+    update.message.reply_text('Help!')
+
+
+def reply_message(update, context):
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    logger.info('starting telegram-good-timing-bot app...')
+
+    updater = telegram.ext.Updater(os.environ.get('BOT_TOKEN'))
+
+    updater.dispatcher.add_handler(telegram.ext.CommandHandler("start", start_command))
+    updater.dispatcher.add_handler(telegram.ext.CommandHandler("help", help_command))
+
+    updater.dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text & ~telegram.ext.Filters.command, reply_message))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
