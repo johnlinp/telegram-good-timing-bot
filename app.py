63ae102506340636bba0ee3ef14312f0df926969
John Lin <johnlinp@gmail.com>
2021-02-13 11:29:10 +0800

    Change polling to webhook

--- app.py
+++ app.py
@@ -35,7 +35,10 @@ def main():
 
     updater.dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text & ~telegram.ext.Filters.command, reply_message))
 
-    updater.start_polling()
+    updater.start_webhook(listen='0.0.0.0',
+            port=os.environ.get('PORT', 8443),
+            url_path=os.environ.get('BOT_TOKEN'))
+    updater.bot.set_webhook('{}{}'.format(os.environ.get('WEBHOOK_BASE_URL'), os.environ.get('BOT_TOKEN')))
 
     updater.idle()
 
