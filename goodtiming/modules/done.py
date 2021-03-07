ab2737415a3183d0791cdd98e78bb8f8c8ea6fed
John Lin <johnlinp@gmail.com>
2021-03-07 12:28:57 +0800

    Only allow marking one thing as done at a time

--- goodtiming/modules/done.py
+++ goodtiming/modules/done.py
@@ -14,7 +14,7 @@ class DoneModule:
         return [DoneProcessor()]
 
     def renderers(self):
-        return [DoneRenderer()]
+        return [PlanNotFoundRenderer(), TooManyPlansRenderer(), PlanDoneRenderer()]
 
 
 class DoneParser:
@@ -38,11 +38,20 @@ class DoneProcessor:
         plan_pattern = request.arguments['plan_pattern']
         current_timing = self._get_current_timing(doer_id)
         matched_plans = self._get_matched_plans(doer_id, current_timing, plan_pattern)
-        self._delete_todo(doer_id, current_timing, plan_pattern)
+        if len(matched_plans) == 0:
+            return Response('PLAN-NOT-FOUND', {
+                'plan_pattern': plan_pattern,
+            })
+        elif len(matched_plans) > 1:
+            return Response('TOO-MANY-PLANS', {
+                'matched_plans': matched_plans,
+            })
 
-        return Response(request.kind, {
-            'plan_pattern': plan_pattern,
-            'matched_plans': matched_plans,
+        matched_plan = matched_plans[0]
+        self._delete_todo(doer_id, current_timing, matched_plan)
+
+        return Response('PLAN-DONE', {
+            'matched_plan': matched_plan,
         })
 
     def _get_current_timing(self, doer_id):
@@ -53,20 +62,30 @@ class DoneProcessor:
         rows = self.database.fetch('SELECT plan FROM todo WHERE doer_id = %s AND timing LIKE %s AND plan LIKE %s', (doer_id, '%{}%'.format(current_timing), '%{}%'.format(plan_pattern)))
         return [row[0] for row in rows]
 
-    def _delete_todo(self, doer_id, current_timing, plan_pattern):
-        self.database.execute('DELETE FROM todo WHERE doer_id = %s AND timing LIKE %s AND plan LIKE %s', (doer_id, '%{}%'.format(current_timing), '%{}%'.format(plan_pattern)))
+    def _delete_todo(self, doer_id, current_timing, matched_plan):
+        self.database.execute('DELETE FROM todo WHERE doer_id = %s AND timing LIKE %s AND plan = %s', (doer_id, '%{}%'.format(current_timing), matched_plan))
 
 
-class DoneRenderer:
+class PlanNotFoundRenderer:
     def render(self, response):
-        if response.kind != 'DONE':
+        if response.kind != 'PLAN-NOT-FOUND':
             return None
 
-        plan_pattern = response.arguments['plan_pattern']
+        return _("I couldn't find anything about {plan_pattern}.").format(plan_pattern=response.arguments['plan_pattern'])
+
+
+class TooManyPlansRenderer:
+    def render(self, response):
+        if response.kind != 'TOO-MANY-PLANS':
+            return None
+
         matched_plans = response.arguments['matched_plans']
-        if len(matched_plans) == 0:
-            return _("I couldn't find anything about {plan_pattern}.").format(plan_pattern=plan_pattern)
-        elif len(matched_plans) == 1:
-            return _('Great! I already marked "{plan}" as done.').format(plan=matched_plans[0])
-        else:
-            return _('Great! I already marked these things as done:\n{plans}').format(plans='\n'.join(matched_plans))
+        return _('There are multiple things I found:\n{plans}\nPlease specify only one at a time.').format(plans='\n'.join(matched_plans))
+
+
+class PlanDoneRenderer:
+    def render(self, response):
+        if response.kind != 'PLAN-DONE':
+            return None
+
+        return _('Great! I already marked "{plan}" as done.').format(plan=response.arguments['matched_plan'])
