cd84742d646c1024eaa2ca442d6d57c8c89bf3e6
John Lin <johnlinp@gmail.com>
2021-03-14 10:24:05 +0800

    Add ReportPastTimingParser and ReportPastTimingRenderer

--- goodtiming/modules/reporttiming.py
+++ goodtiming/modules/reporttiming.py
@@ -9,13 +9,13 @@ import goodtiming.util.stringutil
 
 class ReportTimingModule:
     def parsers(self):
-        return [ReportPresentTimingParser()]
+        return [ReportPresentTimingParser(), ReportPastTimingParser()]
 
     def processors(self):
         return [ReportTimingProcessor()]
 
     def renderers(self):
-        return [ReportPresentTimingRenderer()]
+        return [ReportPresentTimingRenderer(), ReportPastTimingRenderer()]
 
 
 class ReportPresentTimingParser:
@@ -28,12 +28,22 @@ class ReportPresentTimingParser:
         })
 
 
+class ReportPastTimingParser:
+    def parse(self, message):
+        match = re.match(_(r'^i was (?P<timing>.+)$'), message, re.IGNORECASE)
+        if not match:
+            return None
+        return Request('REPORT-PAST-TIMING', {
+            'timing': match.group('timing'),
+        })
+
+
 class ReportTimingProcessor:
     def __init__(self):
         self.database = goodtiming.core.database.Database()
 
     def process(self, request, doer_id):
-        if request.kind != 'REPORT-PRESENT-TIMING':
+        if request.kind != 'REPORT-PRESENT-TIMING' and request.kind != 'REPORT-PAST-TIMING':
             return None
 
         self.database.execute('UPDATE doer SET current_timing = %s WHERE doer_id = %s', (request.arguments['timing'], doer_id))
@@ -56,3 +66,16 @@ class ReportPresentTimingRenderer:
             return _('Go {plan}.').format(plan=plans[0])
         else:
             return _('Go do these things:\n{plans}').format(plans=goodtiming.util.stringutil.format_items(plans))
+
+
+class ReportPastTimingRenderer:
+    def render(self, response):
+        if response.kind != 'REPORT-PAST-TIMING':
+            return None
+        plans = response.arguments['plans']
+        if len(plans) == 0:
+            return _('Nothing to do.')
+        elif len(plans) == 1:
+            return _('You should have done {plan}.').format(plan=plans[0])
+        else:
+            return _('You should have done these things:\n{plans}').format(plans=goodtiming.util.stringutil.format_items(plans))
