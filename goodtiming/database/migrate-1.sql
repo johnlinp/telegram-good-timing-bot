b38d4858dce7e8afef55605e01ef7da547db74fc
John Lin <johnlinp@gmail.com>
2021-04-18 08:39:59 +0800

    Avoid errors in migrate.py

--- goodtiming/database/migrate-1.sql
+++ goodtiming/database/migrate-1.sql
@@ -1,10 +1,10 @@
-CREATE TABLE doer (
+CREATE TABLE IF NOT EXISTS doer (
     doer_id VARCHAR(50) NOT NULL,
     current_timing VARCHAR(500),
     PRIMARY KEY(doer_id)
 );
 
-CREATE TABLE todo (
+CREATE TABLE IF NOT EXISTS todo (
     doer_id VARCHAR(50) NOT NULL,
     timing VARCHAR(500) NOT NULL,
     plan VARCHAR(500) NOT NULL,
